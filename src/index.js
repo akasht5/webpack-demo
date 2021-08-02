8e2ac5db026b9c9a9b0b8ed8b4bda7cd751e9854
Akash Takawale <atakawale207@gmail.com>
2021-08-02 09:20:23 +0530

    add styling loaders to handle sass with bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import AlertService from "./app/alert.service";
 import ComponentService from "./app/component.service";
+import "./main.scss";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
